66032480e116e1d6f6a86d69ab0867c51a1273e0
gabriel <gabriel8819@hotmail.com>
2024-02-18 19:00:18 -0500

    first commit

---
+++ src/main/java/com/vertxcode/ArgocdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vertxcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgocdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgocdDemoApplication.class, args);
+	}
+
+}
