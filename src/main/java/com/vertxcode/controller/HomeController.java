864621f8bdec1346974af84d00ffbc22f30e1760
Gabriel boyer-Beaulieu <gabriel8819@hotmail.com>
2024-02-25 21:33:16 -0500

    version 3

--- src/main/java/com/vertxcode/controller/HomeController.java
+++ src/main/java/com/vertxcode/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
 	@GetMapping("/home")
 	public String getHome() {
-		return "Hello world argo cd!!!!!!!! Test-2";
+		return "Hello world argo cd!!!!!!!! Test-3";
 	}
 	
 }
